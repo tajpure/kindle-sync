0c94fe3af997c0383645a4fd2340faacc1960c4f
tajpure <tajpure@gmail.com>
2015-08-21 00:07:34 +0800

    add cli

--- cli.js
+++ cli.js
@@ -1,10 +1,27 @@
 #!/usr/bin/env node
 
-var argv = require('minimist')(process.argv.slice(2));
-var execshell = require('exec-sh');
+var argv = process.argv.slice(2);
 var sync = require('./app.js');
+var length = argv.length;
 
-if(argv._.length === 0) {
-  console.error('Usage: kindle-sync [directory]');
-  process.exit();
+if(length === 2) {
+  var command = argv[0];
+  var arg = argv[1];
+  switch(command) {
+    case '--config.user' : break;
+    case '--config.pass' : break;
+    case '--config.host' : break;
+    case '--config.port' : break;
+    case '--config.to' : break;
+    case '' : break;
+    default : printHelpInfo();
+  }
 }
+else {
+  printHelpInfo();
+}
+
+function printHelpInfo() {
+  console.error('Usage: kindle-sync <command>\n\nCommands:\n  help     Get help\n ');
+}
+process.exit();
